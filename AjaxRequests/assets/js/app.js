3144464e4041eba637681c4286cc95242b563be0
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-21 10:10:25 +0100

    change

--- AjaxRequests/assets/js/app.js
+++ AjaxRequests/assets/js/app.js
@@ -18,15 +18,15 @@
                 url: "https://cdn.rawgit.com/FDMOliveira/Other-Projects/e77be62/AjaxRequests/assets/data/musicians.json",
                 context: document.body,
                 dataType:'json',
-                success: function(data){
-                    var bg = new Image();
-                    bg.src = jsonObj.pic;  
+                success: function(data){  
                     console.log('Sucess');
                     console.log(data);
                     $('.pic').addClass('in');
                     $('.name').addClass('in-name');
                     $('.quote').addClass('in-quote');
                     var jsonObj = data[id];
+                    var bg = new Image();
+                    bg.src = jsonObj.pic;
                     if (jsonObj.name == "Dave Grohl")
                         document.querySelector('.pic').style.backgroundPosition="62%";                          
                     document.querySelector('.pic').style.backgroundImage = "url(data:image/jpg;base64,"+bg.src+")";
