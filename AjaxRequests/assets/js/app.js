8a38858b0bf0096ab0a88179af5a0d0cdba375e3
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-21 12:38:13 +0100

    change

--- AjaxRequests/assets/js/app.js
+++ AjaxRequests/assets/js/app.js
@@ -8,7 +8,7 @@
             $('.initial-screen#title').removeClass('in');
             setTimeout(() => {
                 getData(id);
-            }, 900);
+            }, 500);
         });
         $.ajaxSetup({ cache: false });
         function getData (id) {
